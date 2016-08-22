3dc6232340c1ea9df20d9dc9a55cc54ee83e44dd
BezobidnijPsih <mulnok07@rambler.ru>
2016-08-23 00:27:47 +0300

    - loading logics changed

--- app/dataservices/album.js
+++ app/dataservices/album.js
@@ -1,11 +1,12 @@
 var _ = require("underscore");
 require("../model/album");
+require("./picture");
 
 var app = angular.module('app');
 var urls = {
     albums: 'http://jsonplaceholder.typicode.com/albums/'
 };
-app.factory('AlbumDataservice', ['$http', 'Album', function ($http, Album) {
+app.factory('AlbumDataservice', ['$http', 'Album', 'PictureDataservice', function ($http, Album, PictureDataservice) {
     //I would prefer to use a separate package called context for this
     //it would be called from the controllers instead of the dataservice
     //it would load the data when called for the first time
@@ -20,7 +21,14 @@ app.factory('AlbumDataservice', ['$http', 'Album', function ($http, Album) {
                     var album = new Album(albumData);
                     result.push(album)
                 });
-                return result;
+                return PictureDataservice.getAll().then(function(data){
+                    _.each(data, function(picture){
+                        var album =_.find(result, {id:picture.albumId});
+                        album.pictures.push(picture);
+                    });
+                    return result;
+                });
+
             })
         }
 
